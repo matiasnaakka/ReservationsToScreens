51abc6254e654eed9d49eaf68641e1712e0334ed
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-05-30 16:11:22 +0300

    fix: correct naming for Info Screen Frontend server and add missing server configurations

--- production/ecosystem.config.cjs
+++ production/ecosystem.config.cjs
@@ -16,7 +16,7 @@ module.exports = {
       },
     },
      {
-      name: 'Info Screen Frontend file server',
+      name: 'Info Screen Frontend  server',
       script: 'infoScreenFrontend.js',
       instances: 1,
       autorestart: true,
@@ -30,6 +30,50 @@ module.exports = {
         NODE_ENV: 'production',
       },
     },
-
+      {
+      name: 'Info Screen Frontend Svelte server',
+      script: 'infoScreenFrontendSvelte.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      cron_restart: '0 0 * * *', // Restart at midnight every day
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'Info Screen rooms management server',
+      script: 'roomsmanagement.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      cron_restart: '0 0 * * *', // Restart at midnight every day
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'Info Screen Api',
+      script: 'infoScreenApi.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      cron_restart: '0 0 * * *', // Restart at midnight every day
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
   ],
 };

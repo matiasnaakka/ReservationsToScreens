de301a02802fc0dfb22614b4ea3484ac23c8192c
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-05-30 16:12:30 +0300

    fix: remove redundant Info Screen API configuration from ecosystem file

--- production/ecosystem.config.cjs
+++ production/ecosystem.config.cjs
@@ -60,20 +60,6 @@ module.exports = {
         NODE_ENV: 'production',
       },
     },
-    {
-      name: 'Info Screen Api',
-      script: 'infoScreenApi.js',
-      instances: 1,
-      autorestart: true,
-      watch: false,
-      max_memory_restart: '1G',
-      cron_restart: '0 0 * * *', // Restart at midnight every day
-      env: {
-        NODE_ENV: 'development',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-      },
-    },
+
   ],
 };

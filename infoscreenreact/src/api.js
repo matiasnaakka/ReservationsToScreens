07eb834bd48dc403118fa92472b102d20019103d
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-05-30 17:30:30 +0300

    fix: add console logs for API base URL and API key for debugging

--- infoscreenreact/src/api.js
+++ infoscreenreact/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3003/";
+console.log("🚀 ~ API_BASE_URL:", API_BASE_URL)
 const API_KEY = import.meta.env.VITE_API_KEY;
+console.log("🚀 ~ API_KEY:", API_KEY)
 
 export const fetchAllRooms = async (floor, startDate, endDate) => {
   try {

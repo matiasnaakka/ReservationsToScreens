da3db79c3c2c019c707030196bf6402d9f724e82
Aleksi Nokelainen <aleksi.nokelainen@gmail.com>
2025-06-02 09:25:44 +0300

    fix: enhance JWT token validation for additional routes

--- production/middleware/auth.js
+++ production/middleware/auth.js
@@ -6,15 +6,15 @@ export default (apiKey) => (req, res, next) => {
   if (req.headers.apikey !== apiKey) {
     return res.status(401).json({message: 'Unauthorized: Invalid API Key'});
   }
-  
+  console.log(req.path)
   // Then check JWT token (except for /api/login and /api/token/validate routes)
-  if (req.path !== '/api/login' && req.path !== '/api/token/validate') {
+  if (req.path !== '/api/login' && req.path !== '/api/token/validate' && req.path !== '/api/rooms/freespace-full' && req.path !== '/api/rooms/reservations' && req.path !== '/api/rooms/freespace' && req.path !== '/api/rooms/all' && req.path !== '/api/campuses/hours') {
     const token = req.headers.authorization?.split(' ')[1];
-    
+
     if (!token) {
       return res.status(401).json({ message: 'Unauthorized: No token provided' });
     }
-    
+
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET || 'default_jwt_secret');
       req.user = decoded;
@@ -22,6 +22,6 @@ export default (apiKey) => (req, res, next) => {
       return res.status(401).json({ message: 'Unauthorized: Invalid token' });
     }
   }
-  
+
   next();
 };
